0b333d7da3173f284b8db7e8973c9c0ba101d2bd
Petersix <150849737+peterarcuri@users.noreply.github.com>
2025-05-02 03:18:06 -0400

    updated package.json & tsconfig.json

--- lyrix-backend/dist/controllers/songController.js
+++ lyrix-backend/dist/controllers/songController.js
@@ -1,17 +1,26 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getLyrics = void 0;
 const song_1 = __importDefault(require("../models/song"));
-const getLyrics = async (req, res) => {
+const getLyrics = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const { query } = req.query;
         let songs;
         if (query) {
             // Case-insensitive search for title or artist
-            songs = await song_1.default.find({
+            songs = yield song_1.default.find({
                 $or: [
                     { title: { $regex: query, $options: 'i' } },
                     { artist: { $regex: query, $options: 'i' } },
@@ -19,7 +28,7 @@ const getLyrics = async (req, res) => {
             });
         }
         else {
-            songs = await song_1.default.find();
+            songs = yield song_1.default.find();
         }
         res.json(songs);
     }
@@ -27,5 +36,5 @@ const getLyrics = async (req, res) => {
         console.error(error);
         res.status(500).json({ message: 'Error fetching songs' });
     }
-};
+});
 exports.getLyrics = getLyrics;
