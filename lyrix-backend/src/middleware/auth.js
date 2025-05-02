50f743902a6c582d91354be78cd70bc011e57dd8
Petersix <150849737+peterarcuri@users.noreply.github.com>
2025-05-02 01:41:06 -0400

    tsconfig.json updated

---
+++ lyrix-backend/src/middleware/auth.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.protect = protect;
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+function protect(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+    const token = authHeader.split(' ')[1];
+    try {
+        const decoded = jsonwebtoken_1.default.verify(token, process.env.JWT_SECRET);
+        req.userId = decoded.id;
+        next();
+    }
+    catch (error) {
+        return res.status(401).json({ message: 'Invalid token' });
+    }
+}
