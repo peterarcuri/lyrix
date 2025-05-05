bfc2c384fa7295f0fe96c46e6e7d0fab1eb1aec8
Petersix <150849737+peterarcuri@users.noreply.github.com>
2025-05-05 02:46:52 -0400

    updated vite.config.ts

--- lyrix-frontend/src/services/api.ts
+++ lyrix-frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: 'https://lyrix.onrender.com//api/v1',
 });
 
 export const signup = (data: { email: string; password: string }) =>
