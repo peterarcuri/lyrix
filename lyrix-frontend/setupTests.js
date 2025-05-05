786be5f5dacd78e06cd0fc897ac5aed77dc80deb
Petersix <150849737+peterarcuri@users.noreply.github.com>
2025-05-04 23:55:21 -0400

    revised tsconfig.json file

--- lyrix-frontend/setupTests.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/// <reference types="node" />
-Object.defineProperty(exports, "__esModule", { value: true });
-require("@testing-library/jest-dom");
-const util_1 = require("util");
-global.TextEncoder = util_1.TextEncoder;
-global.TextDecoder = util_1.TextDecoder;
