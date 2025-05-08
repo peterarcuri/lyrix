859b8419095865915f831024cdd4f3189270ac03
Petersix <150849737+peterarcuri@users.noreply.github.com>
2025-05-08 02:35:02 -0400

    removed js files

--- lyrix-frontend/dist/pages/Home.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { SongSearch } from '../components/SongSearch';
-export default function Home() {
-    return (_jsx("div", { className: "home-container", children: _jsx("div", { className: "home-content", children: _jsx(SongSearch, {}) }) }));
-}
