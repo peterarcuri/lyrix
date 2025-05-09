8e69adc09f572d51d1e26187d564fa29cd614853
Petersix <150849737+peterarcuri@users.noreply.github.com>
2025-05-08 22:23:11 -0400

    modified vite.config.ts file

--- lyrix-frontend/vite.config.ts
+++ lyrix-frontend/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://lyrix.onrender.com//api/v1',
+  base: '/', // ✅ Serve assets relative to site root
   server: {
     proxy: {
       '/api': 'http://localhost:5000',
