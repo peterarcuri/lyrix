4de2b98edfc5afdd8fe80685116b7d3f572a0f96
Petersix <150849737+peterarcuri@users.noreply.github.com>
2025-05-13 02:38:43 -0400

    main.tsx file updated

--- lyrix-frontend/src/main.tsx
+++ lyrix-frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import { createRoot, } from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -9,8 +8,6 @@ import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
     <App />
-    </BrowserRouter>
   </StrictMode>,
 )
