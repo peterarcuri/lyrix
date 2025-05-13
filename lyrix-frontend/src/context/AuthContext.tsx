ead0df649bac647f6189751b4b4561a7dbddd6b7
Petersix <150849737+peterarcuri@users.noreply.github.com>
2025-05-13 00:34:38 -0400

    modified AuthContext file

--- lyrix-frontend/src/context/AuthContext.tsx
+++ lyrix-frontend/src/context/AuthContext.tsx
@@ -22,7 +22,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
     try {
       const res = await fetch('/api/v1/playlists', {
         headers: {
-          Authorization: `Bearer ${token}`,
+          Authorization: `Bearer ${authToken}`,
         },
       });
 
