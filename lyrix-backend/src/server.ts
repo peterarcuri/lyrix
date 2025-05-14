6b5bd639411d8c35cdef7f540d0eabf0ae8b4a9a
Petersix <150849737+peterarcuri@users.noreply.github.com>
2025-05-14 00:49:08 -0400

    server.ts file revised

--- lyrix-backend/src/server.ts
+++ lyrix-backend/src/server.ts
@@ -18,6 +18,8 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = ['https://lyrix-one.vercel.app'];
 
+app.use(express.json());
+
 app.use(cors({
   origin: function (origin, callback) {
     const allowedOrigins = ['https://lyrix-one.vercel.app'];
@@ -31,7 +33,7 @@ app.use(cors({
 }));
 
 
-app.use(express.json());
+
 
 app.get('/', (req, res) => {
   res.send('Lyrix backend says hello! 😁');
