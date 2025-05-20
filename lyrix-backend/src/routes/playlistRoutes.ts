45779935a8bb34b89bf02719834461c1aa9cc135
Petersix <150849737+peterarcuri@users.noreply.github.com>
2025-05-20 02:12:55 -0400

    updated backend files

--- lyrix-backend/src/routes/playlistRoutes.ts
+++ lyrix-backend/src/routes/playlistRoutes.ts
@@ -2,10 +2,11 @@ import express from 'express';
 import { protect } from '../middleware/auth';
 import {
   getUserPlaylists,
-  saveUserPlaylists
+  saveUserPlaylists,
 } from '../controllers/playlistController';
 
 const router = express.Router();
+
 
 router.get('/', protect, getUserPlaylists);
 router.post('/', protect, saveUserPlaylists);
